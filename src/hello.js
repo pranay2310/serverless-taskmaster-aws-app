a1721acb0022261454c03f7031d59fa799cd64fd
Pranaysagar Bhelave <92773034+pranay2310@users.noreply.github.com>
2023-05-15 22:38:27 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Batch 3!",
+        message: "Pranay , you are the best",
       },
       null,
       2
